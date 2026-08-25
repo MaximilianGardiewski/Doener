cf66ff713c4ab2f0333a8cf370451745cb8a35b8
MaximilianGardiewski <120217460+MaximilianGardiewski@users.noreply.github.com>
2026-08-26 01:20:06 +0200

    feat(factory): expose factory public API

---
+++ packages/supabase-factory/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./types.ts";
+export * from "./manifest.ts";
+export * from "./planner.ts";
+export * from "./registry.ts";
+export * from "./provider.ts";
+export * from "./control-plane.ts";
